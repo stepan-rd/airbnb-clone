18dbcc883bc376ed392737f98bdec96d7f3c6623
prdel24 <brozikmrozik7@gmail.com>
2024-07-07 14:56:25 +0200

    chore: Mapped RbnbCards on Home Page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useHomePage } from '@/state/HomePageStore'
+import { generateRandomRbnbs } from '@/utils/generateRandomRbnbs'
+import React, { useEffect } from 'react'
+import { RbnbCard } from './RbnbCard';
 
 type Props = {}
 
 export function HomePage({}: Props) {
+
+  const { allRbnbs, setAllRbnbs } = useHomePage();
+
+
+  useEffect(() => {
+
+    const initialRbnbs = generateRandomRbnbs(500);
+
+    setAllRbnbs(initialRbnbs)
+  }, [])
+
   return (
-    <div>HomePage</div>
+    <div className='flex flex-wrap'>
+      {allRbnbs.map((rbnb, rbnbIndex) => (
+        <RbnbCard key={rbnbIndex} rbnbDetails={rbnb}/>
+      ))}
+    </div>
   )
 }
