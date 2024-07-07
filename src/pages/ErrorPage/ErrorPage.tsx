54ff64d37bba408e1a8be27af1209d75b012955b
prdel24 <brozikmrozik7@gmail.com>
2024-07-07 19:38:50 +0200

    feat: Creating Error Page

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import rbnbLogoSmall from "@/assets/airbnb-small.svg";
+import { useNavigate } from "react-router-dom";
 
-type Props = {}
+type Props = {};
 
 export function ErrorPage({}: Props) {
+  const navigate = useNavigate();
+
   return (
-    <div>ErrorPage</div>
-  )
+    <div className="flex flex-col p-10">
+      <div className="flex">
+        <img src={rbnbLogoSmall} width="32" alt="" />
+        <h1 className="ml-4 text-3xl font-SF-pro">404 Page not found.</h1>
+      </div>
+      <button
+        onClick={() => navigate("/")}
+        className="p-2 transition-all border rounded-lg shadow-md border-stone-200 hover:-translate-y-px active:translate-y-px"
+      >
+        Go Home
+      </button>
+    </div>
+  );
 }
