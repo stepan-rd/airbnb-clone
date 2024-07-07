0ea82458f028404d80a1ae773919b37a26fb1bc3
prdel24 <brozikmrozik7@gmail.com>
2024-07-07 14:57:18 +0200

    chore: Added new RbnbType

---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+export type RbnbType = {
+  imgUrls: string[];
+  name: string;
+  hostedBy: string;
+  pricePerNight: number;
+  stars: number;
+  location: string;
+  rooms: { questCount: number; bedrooms: number; bedCont: number; bathrooms: number };
+};
