1f8b85a72602942ac7fc01dec71b1bfd73094239
prdel24 <brozikmrozik7@gmail.com>
2024-07-07 14:54:25 +0200

    feat: Added function to generate dummy rbnb's

---
+++ src/utils/generateRandomRbnbs.tsx
@@ -0,0 +1,24 @@
+import { RbnbType } from "@/types/types";
+
+export function generateRandomRbnbs(amount: number): RbnbType[] {
+  const results: RbnbType[] = [];
+
+  for (let i = 0; i < amount; i++) {
+    results.push({
+      imgUrls: ["https://placehold.co/376x357"],
+      name: "Lorem Ipsum",
+      hostedBy: "Lorem Ipsum",
+      pricePerNight: Math.random() * 1200 + 60,
+      stars: Math.random() * 1.3 + 3.7,
+      location: "Lorem Ipsum",
+      rooms: {
+        questCount: Math.random() * 10,
+        bedrooms: Math.random() * 5,
+        bedCont: Math.random() * 10,
+        bathrooms: Math.random() * 4,
+      },
+    });
+  }
+
+  return results;
+}
