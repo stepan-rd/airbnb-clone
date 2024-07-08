c08455859f2065aef813ff9cf1d4ed2f25fda71b
prdel24 <brozikmrozik7@gmail.com>
2024-07-08 16:23:28 +0200

    chore: Added some styles and made minor changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,19 +8,21 @@ import { FilterSelectionMedium } from "./FilterSelectionMedium";
 type Props = {};
 
 export function Header({}: Props) {
-
   const navigate = useNavigate();
 
   return (
     <>
       <div className="flex p-4">
-        <div className="flex items-center flex-shrink-0 hover:cursor-pointer" onClick={() => navigate("/")}>
+        <div
+          className="flex items-center flex-shrink-0 hover:cursor-pointer"
+          onClick={() => navigate("/")}
+        >
           <img className="hidden mr-2 sm:block" src={rbnbSvg} width={118} />
-          <img className="block sm:hidden" src={rbnbSvgSmall} width={34} />
+          <img className="block mr-2 sm:hidden" src={rbnbSvgSmall} width={34} />
         </div>
         <div className="justify-center flex-grow hidden md:flex">
           <FilterSelection />
-        </div>        
+        </div>
         <div className="flex justify-center flex-grow md:hidden">
           <FilterSelectionMedium />
         </div>
