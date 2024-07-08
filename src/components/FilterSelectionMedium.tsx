f858b63cfb2221c1ef86caa197b7608536cd4262
prdel24 <brozikmrozik7@gmail.com>
2024-07-08 15:33:23 +0200

    style: Updated size and some text

--- src/components/FilterSelectionMedium.tsx
+++ src/components/FilterSelectionMedium.tsx
@@ -9,16 +9,16 @@ export function FilterSelectionMedium({}: Props) {
   const {theme} = useThemeStore();
 
   return (
-    <div className="relative flex p-2 text-sm leading-4 border rounded-full w-50 shadow-m hover:bg-stone-100 hover:cursor-pointer border-stone-400 whitespace-nowrap">
+    <div className="relative flex p-2 px-4 text-sm leading-4 border rounded-full w-72 shadow-m hover:bg-stone-100 hover:cursor-pointer border-stone-400 whitespace-nowrap">
       <img
         src={searchIcon}
         alt="search-icon"
-        className="ml-3 mr-3 search-icon-black"
+        className="mr-3 search-icon-black"
         width={15}
       />
       <div className="flex flex-col leading-4">
         <h1 className="font-semibold">Where is it going to be?</h1>
-        <h1 className={`${theme.textSecondary}`}>Wherever • Whatever Week • Add Quests</h1>
+        <h1 className={`${theme.textSecondary}`}>Wherever • Add Quests • Whenever</h1>
       </div>
     </div>
   );
