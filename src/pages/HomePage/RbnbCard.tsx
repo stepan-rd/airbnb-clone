d5c6b28801f62d3f5a0643a6da1569236b3a6beb
prdel24 <brozikmrozik7@gmail.com>
2024-07-08 16:21:53 +0200

    chore: Updated Rbnb card to forwardRef

--- src/pages/HomePage/RbnbCard.tsx
+++ src/pages/HomePage/RbnbCard.tsx
@@ -1,24 +1,49 @@
 import { useThemeStore } from "@/state/ThemeStore";
 import { RbnbType } from "@/types/types";
-import React, { useMemo } from "react";
+import React, { forwardRef, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   rbnbDetails: RbnbType;
+  rbnbIndex: number;
 };
 
-export function RbnbCard({ rbnbDetails }: Props) {
+export const RbnbCard = forwardRef<HTMLDivElement, Props>(({ rbnbDetails, rbnbIndex }, ref) => {
   const { theme } = useThemeStore();
 
-  const starImg = useMemo(() => {return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" width="14" role="presentation" focusable="false"><path fill-rule="evenodd" d="m15.1 1.58-4.13 8.88-9.86 1.27a1 1 0 0 0-.54 1.74l7.3 6.57-1.97 9.85a1 1 0 0 0 1.48 1.06l8.62-5 8.63 5a1 1 0 0 0 1.48-1.06l-1.97-9.85 7.3-6.57a1 1 0 0 0-.55-1.73l-9.86-1.28-4.12-8.88a1 1 0 0 0-1.82 0z"></path></svg>}, [])
+  const navigate = useNavigate();
+
+  const starImg = useMemo(() => {
+    console.log(`img number: ${rbnbIndex} is rerendering...`)
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 32 32"
+        aria-hidden="true"
+        width="14"
+        role="presentation"
+        focusable="false"
+      >
+        <path
+          fillRule="evenodd"
+          d="m15.1 1.58-4.13 8.88-9.86 1.27a1 1 0 0 0-.54 1.74l7.3 6.57-1.97 9.85a1 1 0 0 0 1.48 1.06l8.62-5 8.63 5a1 1 0 0 0 1.48-1.06l-1.97-9.85 7.3-6.57a1 1 0 0 0-.55-1.73l-9.86-1.28-4.12-8.88a1 1 0 0 0-1.82 0z"
+        ></path>
+      </svg>
+    );
+  }, []);
 
   return (
-    <div className="m-10 w-fit">
+    <div
+      className="m-10 transition-transform duration-300 transform w-fit hover:cursor-pointer"
+      onClick={() => navigate(`/details/${rbnbIndex}`)}
+      ref={ref}
+    >
       <img
         src={rbnbDetails.imgUrls[0]}
         className="rounded-2xl"
         alt="rbnb-img"
       />
-      <div className="relative">
+      <div className="relative mt-2">
         <h1 className={`${theme.textMain} font-semibold`}>
           {rbnbDetails.name}
         </h1>
@@ -32,10 +57,12 @@ export function RbnbCard({ rbnbDetails }: Props) {
           <h1 className="ml-1">per night</h1>
         </div>
         <div className="absolute top-0 right-0 flex items-center justify-center">
-            <h1 className="mr-1">{starImg}</h1>
-           {rbnbDetails.stars.toFixed(2)}
+          <h1 className="mr-1">{starImg}</h1>
+          {rbnbDetails.stars.toFixed(2)}
         </div>
       </div>
     </div>
   );
-}
+});
+
+RbnbCard.displayName = "RbnbCard";
