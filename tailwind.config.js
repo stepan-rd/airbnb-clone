65e9d4a03a9a3667ed42e45c5080e633c52706d5
prdel24 <brozikmrozik7@gmail.com>
2024-07-14 12:39:17 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,30 @@ export default {
 
       fontFamily: {
         "SF-pro": ["SF pro"]
-      }
+      },
+
+      minHeight: {
+        '9.375rem': '9.375rem', // 12.5rem fits between 48 (12rem) and 56 (14rem)
+      },
+      minWidth: {
+        '9.375rem': '9.375rem', // 12.5rem fits between 48 (12rem) and 56 (14rem)
+      },
+
+      maxWidth: {
+        '63.75rem': '63.75rem'
+      },
+
+      maxHeight: {
+        '32.188rem': '32.188rem'
+      },
+
+      scale: {
+        '102': '1.02'
+      },
+
+      fontSize: {
+        '10px': '10px'
+      },
     },
   },
   plugins: [],
