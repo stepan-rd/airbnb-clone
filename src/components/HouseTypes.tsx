d5efafbb60293da2a813d1741707d36354058b14
prdel24 <brozikmrozik7@gmail.com>
2024-07-14 12:34:35 +0200

    feat: Added house type selection

--- src/components/HouseTypes.tsx
+++ src/components/HouseTypes.tsx
@@ -15,7 +15,7 @@ export function HouseTypes({}: Props) {
   const { hoveredIndex, activeIndex } = useHouseTypeSelect();
   const [scrollIndex, setScrollIndex] = useState(0);
   const [currScreenWidth, setCurrScreenWidth] = useState(0);
-  const [houseTypesContainerWidth, setHouseTypesContainerWidth] = useState(0)
+  const [houseTypesContainerWidth, setHouseTypesContainerWidth] = useState(0);
 
   const houseTypesContainerRef = useRef<HTMLDivElement | null>(null);
   const houseTypesWrapperRef = useRef<HTMLDivElement | null>(null);
@@ -30,39 +30,43 @@ export function HouseTypes({}: Props) {
     if (houseTypesWrapperRef.current) {
       setHouseTypesContainerWidth(houseTypesWrapperRef.current.offsetWidth);
     }
-  }, [houseTypesWrapperRef.current?.offsetWidth])
+  }, [houseTypesWrapperRef.current?.offsetWidth]);
 
   useEffect(() => {
     console.log(scrollIndex);
   }, [scrollIndex]);
 
   return (
-    <div
-      className="relative px-5 mt-5 ml-5 overflow-hidden h-14"
-      ref={houseTypesContainerRef}
-    >
+    <div className="fixed z-50 w-full bg-white border-b shadow-sm border-b-stone-200" style={{top: "83px"}}>
       <div
-        className="absolute flex transition-all duration-700"
-        ref={houseTypesWrapperRef}
-        style={{ left: `${scrollIndex * (currScreenWidth * 0.7)}px` }}
+        className="relative px-5 mt-4 mb-2 ml-5 overflow-hidden h-14"
+        ref={houseTypesContainerRef}
       >
-        {houseTypes.map((house, index) => (
-          <MemoHouseType
-            key={index}
-            house={house}
-            isHovered={hoveredIndex === index}
-            isActive={activeIndex === index}
-            index={index}
-          />
-        ))}
-      </div>
-      {scrollIndex !== 0 && (
-        <ArrowLeftBtn onClick={() => setScrollIndex((prev) => prev + 1)} />
-      )}
+        <div
+          className="absolute flex transition-all duration-700"
+          ref={houseTypesWrapperRef}
+          style={{ left: `${scrollIndex * (currScreenWidth * 0.7)}px` }}
+        >
+          {houseTypes.map((house, index) => (
+            <MemoHouseType
+              key={index}
+              house={house}
+              isHovered={hoveredIndex === index}
+              isActive={activeIndex === index}
+              index={index}
+            />
+          ))}
+        </div>
+        {scrollIndex !== 0 && (
+          <ArrowLeftBtn onClick={() => setScrollIndex((prev) => prev + 1)} />
+        )}
 
-      {currScreenWidth && Math.abs(scrollIndex) * currScreenWidth < (houseTypesContainerWidth + currScreenWidth / 5) && (
-        <ArrowRightBtn onClick={() => setScrollIndex((prev) => prev - 1)} />
-      )}
+        {currScreenWidth &&
+          Math.abs(scrollIndex) * currScreenWidth <
+            houseTypesContainerWidth + currScreenWidth / 5 && (
+            <ArrowRightBtn onClick={() => setScrollIndex((prev) => prev - 1)} />
+          )}
+      </div>
     </div>
   );
 }
