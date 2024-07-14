0734367b46a060e6d7b8dfcb5bf3657854ea61b4
prdel24 <brozikmrozik7@gmail.com>
2024-07-14 12:39:15 +0200

    Update router.tsx

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -1,6 +1,7 @@
 import App from "@/App";
 import { ErrorPage } from "@/pages/ErrorPage/ErrorPage";
 import { HomePage } from "@/pages/HomePage/HomePage";
+import RbnbDetailsPage from "@/pages/RbnbDetailsPage/RbnbDetailsPage";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
@@ -8,6 +9,9 @@ export const router = createBrowserRouter([
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
-    children: [{ path: "/", element: <HomePage /> }],
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/details/:rbnbId", element: <RbnbDetailsPage /> },
+    ],
   },
 ]);
