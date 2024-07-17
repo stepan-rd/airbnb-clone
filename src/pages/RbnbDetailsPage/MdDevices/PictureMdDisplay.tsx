6dddd54a64a3451bee5b736b97e6730d6679fa8d
prdel24 <brozikmrozik7@gmail.com>
2024-07-17 16:54:33 +0200

    idk idc

--- src/pages/RbnbDetailsPage/MdDevices/PictureMdDisplay.tsx
+++ src/pages/RbnbDetailsPage/MdDevices/PictureMdDisplay.tsx
@@ -1,8 +1,13 @@
-
-type Props = {}
+type Props = {};
 
 export function PictureMdDisplay({}: Props) {
   return (
-    <div className="">PictureMdDisplay</div>
-  )
+    <div className="min-h-screen" style={{ marginTop: "83px" }}>
+      <div className="relative overflow-y-hidden">
+        <img src="https://placehold.co/600x400" className="w-full" alt="" />
+        <button className="absolute left-0 text-lg font-semibold top-1/2">{"<"}</button>
+        <button className="absolute right-0 text-lg font-semibold top-1/2">{">"}</button>
+      </div>
+    </div>
+  );
 }
